f0d6631ba80cae42c030e6e52a844b9b7ce8cc86
Greg <gbugaj@rmsweb.com>
2020-06-09 17:12:10 -0500

    WIP

--- data/flickr/preprocess.py
+++ data/flickr/preprocess.py
@@ -12,6 +12,28 @@ from os import listdir
 from os.path import isfile, join
 from os import walk
 
+import matplotlib.pyplot as plt
+
+# Dependency update
+# python3 -m pip install --upgrade pip
+# python3 -m pip install  opencv-python
+# python3 -m pip install  python3 -m pip install  matplotlib
+
+
+# Once the images are loaded, we need to ensure the images are of the same size. 
+# We will resize all the images to be 224 * 224 pixels. 
+# Let’s flip 50% of the training data set horizontally and crop them to 224 * 224 pixels
+train_augs = [
+    mx.image.HorizontalFlipAug(.5),
+    mx.image.RandomCropAug((224, 224))
+]
+
+# For the validation and test data sets, let’s center crop to get each image to 224 224. 
+# All the images in the train, test, and validation data sets will now be of 224 224 size.
+val_test_augs = [
+    mx.image.CenterCropAug((224, 224))
+]
+
 def extractLogosToFolders():
     print("Image training set segregation")
     annotations = open('flickr_logos_27_dataset_training_set_annotation.txt', 'r') 
@@ -28,6 +50,7 @@ def extractLogosToFolders():
         print ("Line {} : {} == {}".format(count, line, parts))
 
         # 144503924.jpg Adidas 1 38 12 234 142 
+        # 2662264721.jpg RedBull 2 3 197 3 197
         name = parts[0]
         label = parts[1].lower()    
         clazz = int(parts[2])
@@ -38,18 +61,27 @@ def extractLogosToFolders():
         w = x2 - x1
         h = y2 - y1
 
+        print (parts)
+        print (" {} : {} ==  {} : {} ".format(x1, y1, x2, y2))
+        print (" {} : {} ".format(w, h))
+
+        # 2662264721.jpg RedBull 2 3 197 3 197
+        if w == 0 or h == 0:
+            continue
+
         imagePath = os.path.join("./flickr_logos_27_dataset_images", name)
         labelDir = os.path.join("./dataset", label)
         imageDestName = os.path.join("./dataset", label, name)
         if not os.path.exists(labelDir):
             os.makedirs(labelDir)
 
-        print (labelDir)
         img = cv2.imread(imagePath)
         crop = img[y1:y1+h, x1:x1+w]
+
         #cv2.imshow('Image', crop)
         #cv2.waitKey(100)
         print(imageDestName)
+        # print (img)
         cv2.imwrite(imageDestName, crop)
 
 def copyfiles(files, srcDir, destDir):    
@@ -111,15 +143,78 @@ def separateTrainingSet():
         copyfiles(validation_files, clazzDir, valSetData)
         copyfiles(testing_files, clazzDir, testSetData)
 
-def transform(srcImage):
+def transform(data, label, augs):
+    data = data.astype('float32')
+    for aug in augs:
+        data = aug(data)
+    # from (H x W x c) to (c x H x W)
+    data = mx.nd.transpose(data, (2, 0, 1))
+    return data, mx.nd.array(([label])).asscalar().astype('float32')
+
+
+def imageInfo(srcImage):
     print("image : {}".format(srcImage))
     img = cv2.imread(srcImage)
     cv2.imshow('Image', img)
     cv2.waitKey(10000)
-    
+ 
+def show_images(imgs, nrows, ncols, figsize=None):
+
+    print(imgs)
+    """plot a grid of images"""
+    figsize = (ncols, nrows)
+    _, figs = plt.subplots(nrows, ncols, figsize=figsize)
+    for i in range(nrows):
+        for j in range(ncols):
+            figs[i][j].imshow(imgs[i*ncols+j].asnumpy())
+            figs[i][j].axes.get_xaxis().set_visible(False)
+            figs[i][j].axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+def train():
+    print("Training")    
+
+    # MXNetError: [11:31:23] ../src/io/batchify.cc:128: Check failed: ashape == inputs[j][i].shape() ([394,395,3] vs. [36,49,3])
+    # StackBatchify requires all data along batch dim to be the same, mismatch [394,395,3] vs. [36,49,3]
+
+    batch_size = 1
+    num_classes = 3
+    num_epochs = 1
+    num_gpu = 1
+    ctx = mx.cpu()#[mx.gpu(i) for i in range(num_gpu)]
+    print (ctx)
+
+    # performs the transformation on the data and returns the updated data set
+    data_directory = './trainingset/'
+    # Transform the image when loading
+    train_imgs = mx.gluon.data.vision.ImageFolderDataset(data_directory+'train_data', transform=lambda X, y: transform(X, y, train_augs))
+   # test_imgs  = mx.gluon.data.vision.ImageFolderDataset(data_directory+'test_data', transform=lambda X, y: transform(X, y, val_test_augs))
+    #val_imgs   = mx.gluon.data.vision.ImageFolderDataset(data_directory+'val_data', transform=lambda X, y: transform(X, y, val_test_augs))
+   
+    # print(train_imgs)
+    # print(test_imgs)
+   #  print(val_imgs)
+
+    train_data = mx.gluon.data.DataLoader(train_imgs, batch_size, num_workers=1, shuffle=True)
+   # val_data = mx.gluon.data.DataLoader(val_imgs, batch_size, num_workers=1)
+    #test_data = mx.gluon.data.DataLoader(test_imgs, batch_size, num_workers=1)
+       
+    print(train_data)
+    # Now, display the first 32 images in a 8 * 4 grid:
+    for X, _ in train_data:
+        # from (B x c x H x W) to (Bx H x W x c)
+        #Y = X.transpose((0, 2, 3, 1)).clip(0, 255)/255
+        print (X)
+        #show_images(X, 4, 8)
+        break
+
+   # print(val_data)
+   # print(test_data)
+
 if __name__ == "__main__":
-#    extractLogosToFolders()
-    #separateTrainingSet()
-    transform("./trainingset/test_data/yahoo/3002734592.jpg")
+    #extractLogosToFolders()
+    #separateTrainingSet()    
+    train()
 
 
